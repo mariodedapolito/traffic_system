c1e7b1e27096b16a2802574b6c468a41f1f3ab4a
Stefano Giannuzzi <s287549@studenti.polito.it>
2021-09-19 17:38:51 +0200

    added path finding to the cars in Hybrid-ECS + fixed some bugs

--- Assets/Scripts/System/CarSystem.cs
+++ Assets/Scripts/System/CarSystem.cs
@@ -16,9 +16,58 @@ class CarSystem : SystemBase
             .WithoutBurst()
             .ForEach((DynamicBuffer<ListNode> ListNode, ref CarPosition position, ref CarDestination destination, ref Translation translation, ref Rotation rotation, ref VehicleSpeed speed, in LocalToWorld ltw) =>
             {
-                
-                if (position.currentNode >= ListNode.Length) 
+
+                if (position.currentNode == ListNode.Length - 1)
+                {
+                    Node startWaypoint = new Node(), endWaypoint = new Node();
+
+                    GameObject[] waypointsNew = GameObject.FindGameObjectsWithTag("CarWaypoint");
+                    List<Node> nodesNew = new List<Node>();
+                    foreach (GameObject w in waypointsNew)
+                    {
+                        if (w.GetComponent<Node>() != null && !w.GetComponent<Node>().isParkingSpot)
+                        {
+                            if (ListNode[ListNode.Length - 1].listNodesTransform.Equals((float3)w.transform.position)) //find start waypoint
+                            {
+                                startWaypoint = w.GetComponent<Node>();
+                            }
+                            /*
+                            if (ListNode[ListNode.Length - 1].listNodesTransform.Equals((float3)w.transform.position)) //find end waypoint
+                            {
+                                endWaypoint = w.GetComponent<Node>();
+                            }*/
+
+                            nodesNew.Add(w.GetComponent<Node>());
+                        }
+                    }
+
+                    int randomSrcNode = (int)UnityEngine.Random.Range(0, nodesNew.Count - 1);
+
+                    /*Street s = nodesNew[randomSrcNode].GetComponentInParent<Street>();
+
+                    while (!s.hasBusStop && s.isSemaphoreIntersection && s.isSimpleIntersection)
+                    {
+                        randomSrcNode = (int)UnityEngine.Random.Range(0, nodesNew.Count - 1);
+                        s = nodesNew[randomSrcNode].GetComponentInParent<Street>();
+                    }*/
+
+                    endWaypoint = nodesNew[randomSrcNode];
+
+                    /** NEW PATH **/
+                    Path path = new Path();
+                    List<Node> carPath = new List<Node>();
+                    
+                    carPath = path.findShortestPath(startWaypoint.transform, endWaypoint.transform);
+                    ListNode.Clear();
+
+                    for (int i = 0; i < carPath.Count; i++)
+                    {
+                        ListNode.Add(new ListNode { listNodesTransform = carPath[i].transform.position});
+
+                    }
+
                     position.currentNode = 0;
+                }
 
                 position.carPosition = translation.Value;
 
